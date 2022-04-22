e0391cd5d2c76298c660e655cbfb333a280ba428
omkarjajula <91770052+omkarjajula@users.noreply.github.com>
2022-04-22 09:21:54 +0530

    Update luser-demo04.sh

--- luser-demo04.sh
+++ luser-demo04.sh
@@ -6,4 +6,5 @@ read -p "please enter your name " NAME
 read -p "enter the comment for this name " COMMENT
 read -p "enter password " PASS
 useradd -c "$COMMENT" -m $NAME
-echo $PASS | passwd --stdin $NAME
+echo $PASS
+echo ${PASS} | passwd --stdin ${NAME}
