b15c4f3115e7a16d0d32e0e6fd56a7376e4a608b
omkarjajula <91770052+omkarjajula@users.noreply.github.com>
2022-04-22 09:08:37 +0530

    Update luser-demo04.sh

--- luser-demo04.sh
+++ luser-demo04.sh
@@ -3,4 +3,6 @@
 #script create an account on local system prompted for account name and password
 #ask use name
 read -p "please enter your name " NAME
-echo ${NAME}
+read -p "enter the comment for this name " COMMENT
+read -p "enter password " PASS
+user add -c "$COMMENT" -m $NAME
