279dad42391bd28e12e44d55e0f4a2fbb92a96db
root <root@ip-172-31-7-92.ap-south-1.compute.internal>
2022-04-22 00:22:47 +0000

    commit 2

--- luser-demo02.sh
+++ luser-demo02.sh
@@ -4,4 +4,15 @@
 #display if the user is the root or not
 
 
-echo "your uid is ${UID}"
+   echo "your uid is ${UID}"
+ #  USER_NAME=$(id -un)
+   USER_NAME=`id -un`
+   echo "your name is ${USER_NAME}"
+
+   if [[ "${UID}" -eq 0 ]]
+   then
+	   echo 'you are root'
+   else
+	   echo 'you are not root'
+   fi
+
