1f334a4548ea7dbb7b819ed542b53c690df3f180
root <root@ip-172-31-7-92.ap-south-1.compute.internal>
2022-04-22 15:13:06 +0000

    last commit of 22 apr

---
+++ luser-demo06.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "you executed this command: ${0}"
+
+
+echo "you used $(dirname ${0}) as the path to the $(basename ${0}) script"
+
+
+echo "you supplied ${#} arguments on the command line"
