7ebddcff8199d20c90a41bbea7810c75f1825852
omkarjajula <91770052+omkarjajula@users.noreply.github.com>
2022-04-22 08:55:31 +0530

    Update luser-demo04.sh

--- luser-demo04.sh
+++ luser-demo04.sh
@@ -2,4 +2,5 @@
 
 #script create an account on local system prompted for account name and password
 #ask use name
-read -p "please enter your name "
+read -p "please enter your name " NAME
+echo ${NAME}
